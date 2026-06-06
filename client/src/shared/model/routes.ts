c0efaa7a5fd754f00e9280bdbb5c2823437a3f26
Dmytro Orikhovskyi <dmytro.orikhovskyi@lembergsolutions.com>
2026-06-06 20:26:27 +0300

    add react router packages

---
+++ client/src/shared/model/routes.ts
@@ -0,0 +1,24 @@
+import 'react-router-dom';
+
+export const ROUTES = {
+  HOME: '/',
+  LOGIN: '/login',
+  REGISTER: '/register',
+  RESET_PASSWORD: '/reset-password',
+  PROFILE: '/profile',
+  PRODUCTS: '/products',
+  PRODUCT: '/products/:slug',
+} as const;
+
+export interface PathParams {
+  [ROUTES.PRODUCT]: {
+    slug: string;
+  }
+}
+
+
+declare module 'react-router-dom' {
+  interface Register {
+    params: PathParams;
+  }
+}
